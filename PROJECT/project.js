62efecc2995c3a59d5142d40a0d88f9ada6fc815
Duongduchuynh <duchuynh4799@gmail.com>
2020-11-02 18:17:47 +0700

    project

--- PROJECT/project.js
+++ PROJECT/project.js
@@ -1,56 +1,91 @@
-// /*!
-//     * Start Bootstrap - Agency v6.0.2 (https://startbootstrap.com/template-overviews/agency)
-//     * Copyright 2013-2020 Start Bootstrap
-//     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-//     */
-//     (function ($) {
-//     "use strict"; // Start of use strict
+jQuery(document).ready(function ($) {
+    		 $('.counter').counterUp({
+         delay: 10,
+         time: 5000
+		     })
+		 });
+	(function($) {
+   	 function headerStyle() {
+        if($('body').length){
+            var windowpos = $(window).scrollTop();
+            var scrollLink = $('.scroll-top');
+            if (windowpos >= 250) {
+                scrollLink.addClass('open');
+            } else {
+                scrollLink.removeClass('open');
+            }
+        }
+    	} 
+    	headerStyle(); 
+    	 if($('.scroll-to-target').length){
+    	 	    		  $(".scroll-to-target").on('click', function() {
+            var target = $(this).attr('data-target');
+           // animate
+           $('html, body').animate({
+               scrollTop: $(target).offset().top
+             }, 1000);
+    
+        });
+    	 }
+	    $(window).on('scroll', function() {
+	        headerStyle();
+	    }); 
+	})(window.jQuery);
 
-//     // Smooth scrolling using jQuery easing
-//     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-//         if (
-//             location.pathname.replace(/^\//, "") ==
-//                 this.pathname.replace(/^\//, "") &&
-//             location.hostname == this.hostname
-//         ) {
-//             var target = $(this.hash);
-//             target = target.length
-//                 ? target
-//                 : $("[name=" + this.hash.slice(1) + "]");
-//             if (target.length) {
-//                 $("html, body").animate(
-//                     {
-//                         scrollTop: target.offset().top - 72,
-//                     },
-//                     1000,
-//                     "easeInOutExpo"
-//                 );
-//                 return false;
-//             }
-//         }
-//     });
+	if (window.innerWidth > 1000) {
+        $(window).scroll(function () {
+            if ($(window).scrollTop() >=123) {
+                $('.navbar-custom').addClass('fixed_bn');
+                
+            }   
+            else {
+                $('.navbar-custom').removeClass('fixed_bn');
+                
+            }
+        });
+   	 }  
 
-//     // Closes responsive menu when a scroll trigger link is clicked
-//     $(".js-scroll-trigger").click(function () {
-//         $(".navbar-collapse").collapse("hide");
-//     });
 
-//     // Activate scrollspy to add active class to navbar items on scroll
-//     $("body").scrollspy({
-//         target: "#mainNav",
-//         offset: 74,
-//     });
 
-//     // Collapse Navbar
-//     var navbarCollapse = function () {
-//         if ($("#mainNav").offset().top > 100) {
-//             $("#mainNav").addClass("navbar-shrink");
-//         } else {
-//             $("#mainNav").removeClass("navbar-shrink");
-//         }
-//     };
-//     // Collapse now if page is not at top
-//     navbarCollapse();
-//     // Collapse the navbar when page is scrolled
-//     $(window).scroll(navbarCollapse);
-// })(jQuery); // End of use strict
+
+
+
+
+   	 
+   
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+			  .not('[href="#"]')
+			  .not('[href="#0"]')
+			  .click(function(event) {
+			    // On-page links
+			    if (
+			      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+			      && 
+			      location.hostname == this.hostname
+			    ) {
+			      // Figure out element to scroll to
+			      var target = $(this.hash);
+			      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			      // Does a scroll target exist?
+			      if (target.length) {
+			        // Only prevent default if animation is actually gonna happen
+			        event.preventDefault();
+			        $('html, body').animate({
+			          scrollTop: target.offset().top
+			        }, 1000, function() {
+			          // Callback after animation
+			          // Must change focus!
+			         // var $target = $(target);
+			         // $target.focus();
+			         //  if ($target.is(":focus")) { // Checking if the target was focused
+			         //    return false;
+			         //  } else {
+			         //    $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+			         //    $target.focus(); // Set focus again
+			         //  };
+			        });
+			      }
+			    }
+			  });
+
